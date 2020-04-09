acf424bf8f14e28e05b5eefb9297f5e3d67714b0
Ryan O'Shea <rpo1202@gmail.com>
2020-04-09 02:01:05 -0400

    K means segmentation working but quite slow

--- hw2_image_segmentation/image_segmentation.py
+++ hw2_image_segmentation/image_segmentation.py
@@ -19,9 +19,9 @@ import time
 # Returns the Euclidean distance between two RGB triplets
 def getColorDist(r1, g1, b1, r2, g2, b2):
 	# Get individual color distances squared
-	red_dist = (int(r1)-int(r2))**2
-	green_dist = (int(g1)-int(g2))**2
-	blue_dist = (int(b1)-int(b2))**2
+	red_dist = (float(r1)-float(r2))**2
+	green_dist = (float(g1)-float(g2))**2
+	blue_dist = (float(b1)-float(b2))**2
 
 	distance = math.sqrt(red_dist + green_dist + blue_dist)
 
@@ -32,73 +32,121 @@ def kMeansClustering(img, k=10):
 	# Get 10 random pixels from the image and store them in a list
 	centers = []
 
-	# Loop until 10 points have been collected. These are our cluster centers
-	for i in range(10):
+	# Loop until 10 points have been collected. These are our starting cluster centers
+	while len(centers) < 10:
 		# First random number
 		x = random.randint(0, img.shape[1] - 1)
 
 		# Second random number
 		y = random.randint(0, img.shape[0] - 1)
 
-		# Append the coordinate pair to the list
-		centers.append((x, y))
+		# If the point picked has not yet been picked as a center add it to the list
+		if [x, y, img[y][x][2], img[y][x][1], img[y][x][0]] not in centers:
+			# Append the coordinate pair and its RGB value to the list
+			centers.append([x, y, img[y][x][2], img[y][x][1], img[y][x][0]])
 
 	print(centers)
 
 	# List of lists to hold the K clusters
 	clusters = []
 
-	for i in range(k):
-		# Append an empty list
-		clusters.append([])
+	converged = False
 
+	# Loop until clusters have converged
+	while(converged == False):
+		# Clear clusters
+		clusters = []
 
-	# Loop through the image and assign each pixel to its closest cluster center according to color
-	for i in range(img.shape[0]):
-		for j in range(img.shape[1]):
-			# Distance variable for holding distance between pixel color and cluster center color
-			# Reset to very high value for each pixel
-			dist = 10000000
+		for i in range(k):
+			# Append an empty list
+			clusters.append([])
 
-			# Index variable to keep track of which cluster the pixel best fits in
-			idx = 0
+		# Loop through the image and assign each pixel to its closest cluster center according to color
+		for i in range(img.shape[0]):
+			for j in range(img.shape[1]):
+				# Distance variable for holding distance between pixel color and cluster center color
+				# Reset to very high value for each pixel
+				dist = 10000000
 
-			# Check distance between pixel color and colors of each color center
-			for x in range(len(clusters)):
-				# Get coordinates of a cluster center
-				center = centers[x]
+				# Index variable to keep track of which cluster the pixel best fits in
+				idx = 0
 
-				# print(center[0])
-				# print(center[1])
+				# Check distance between pixel color and colors of each color center
+				for x in range(len(clusters)):
+					# Get coordinates of a cluster center
+					center = centers[x]
 
-				# Get the individual colors values for the center. OpenCV stores images as BGR by default
-				# Index images using the coordinate pair as (y, x) or (rows, cols)
-				cent_r = img[center[1]][center[0]][2]
-				cent_g = img[center[1]][center[0]][1]
-				cent_b = img[center[1]][center[0]][0]
+					# Get the individual colors values for the center.
+					cent_r = center[2]
+					cent_g = center[3]
+					cent_b = center[4]
 
-				# print(str(cent_r) + ' ' + str(cent_g) + ' ' + str(cent_b))
+					# Get the individual colors values for the pixel. OpenCV stores images as BGR by default
+					# Index images using the coordinate pair as (y, x) or (rows, cols)
+					pix_r = img[i][j][2]
+					pix_g = img[i][j][1]
+					pix_b = img[i][j][0]
 
-				# Get the individual colors values for the pixel
-				pix_r = img[i][j][2]
-				pix_g = img[i][j][1]
-				pix_b = img[i][j][0]
+					new_dist = getColorDist(cent_r, cent_g, cent_b, pix_r, pix_g, pix_b)
 
-				new_dist = getColorDist(cent_r, cent_g, cent_b, pix_r, pix_g, pix_b)
+					# Check if new color distance is smaller than current smallest color distance to a center
+					if new_dist < dist:
+						# Set new smallest distance and new cluster index that the pixel best belongs to
+						dist = new_dist
+						idx = x
 
-				# Check if new color distance is smaller than current smallest color distance to a center
-				if new_dist < dist:
-					# Set new smallest distance and new cluster index that the pixel best belongs to
-					dist = new_dist
-					idx = x
+				# Add the pixel to the cluster that it was closest to
+				clusters[idx].append([j, i, img[i][j][2], img[i][j][1], img[i][j][0]])
+		
+		# Loop through clusters to find the new ideal cluster center
+		for i in range(len(clusters)):
+			cluster = clusters[i]
 
-			# Add the pixel to the cluster that it was closest to
-			clusters[idx].append(((i, j)))
+			# Variables to hold average pixel colors
+			avg_r = 0
+			avg_g = 0
+			avg_b = 0
+
+			# Find the average of each color in the cluster
+			for j in range(len(cluster)):
+				avg_r += cluster[j][2]
+				avg_g += cluster[j][3]
+				avg_b += cluster[j][4]
+
+			avg_r = int(avg_r/len(cluster))
+			avg_g = int(avg_g/len(cluster))
+			avg_b = int(avg_b/len(cluster))
+
+			# Check if the average color for the cluster changed
+			if avg_r != centers[i][2] or avg_g != centers[i][3] or avg_b != centers[i][4]:
+				centers[i][2] = avg_r
+				centers[i][3] = avg_g
+				centers[i][4] = avg_b
+			# If the center didn't change then he clusters have converged
+			else:
+				converged = True
+
+	# Segmented image to return
+	ouput = img.copy()
+
+	print("New centers: " + str(centers))
+
+	# Loop through clusters and change value of all pixels in that cluster to the cluster center
+	for x in range(len(clusters)):
+		cluster = clusters[x]
+		center = centers[x]
+
+		# Loop through points in cluster
+		for point in cluster:
+			ouput[point[1]][point[0]][2] = center[2] # Set Red
+			ouput[point[1]][point[0]][1] = center[3] # Set Green
+			ouput[point[1]][point[0]][0] = center[4] # Set Blue
+
+	cv2.imshow("Done?", ouput)
+	cv2.waitKey(0)
+
 	
-	for clust in clusters:
-		print(clust)
-		cv2.imshow("Reeeee", img)
-		cv2.waitKey(0)
+
 
 
 if __name__ == "__main__":
