56784b788fb6d6cdea3ee66afc52380f5dc4a751
Ryan O'Shea <rpo1202@gmail.com>
2020-04-28 02:17:49 -0400

    Initial commit of homework 3 image classification

---
+++ hw3_image_classification/image_classification.py
@@ -0,0 +1,11 @@
+"""
+	Ryan O'Shea
+
+	Description: Computer Vision Homework 3 Image classification. 
+	1. classify each image of the test set into one of three classes: coast, forest or “insidecity” using histograms
+	2. Classify individual pixels in an image as sky or non sky
+
+	I pledge my honor that I have abided by the Stevens Honor System
+"""
+
+import cv2
