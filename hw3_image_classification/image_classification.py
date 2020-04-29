3ef8feaf44d168aad9fda338c57208746e049ce0
Ryan O'Shea <rpo1202@gmail.com>
2020-04-28 23:25:09 -0400

    Working histogram classifier with ~83% accuracy

--- hw3_image_classification/image_classification.py
+++ hw3_image_classification/image_classification.py
@@ -9,7 +9,8 @@
 """
 
 import cv2
-import os
+import glob
+import math
 
 # Gets the histogram of the passed in image
 def getHist(img, bins):
@@ -39,7 +40,12 @@ def getHist(img, bins):
 			hist[int(green // (256/bins))][1] += 1
 			hist[int(red // (256/bins))][2] += 1
 
-	return hist
+	# Check to make sure there are the proper amount of histogram entries
+	if verifyHist(hist, img):
+		return hist
+	else:
+		print("Error: Histogram did not verify")
+		return None
 
 # Verifies that the histogram of the passed in image has the same number of pixels in the image times 3
 def verifyHist(hist, img):
@@ -63,17 +69,102 @@ def verifyHist(hist, img):
 	else:
 		return False
 
+# Gets the N dimensional Euclidian distance between two histograms where N is the number 
+# of bins in the histogram
+def getEuclidDist(hist1, hist2):
+	bins = len(hist1)
+
+	dist = 0
+
+	# Loop through all the elements to determine the distance
+	for i in range(len(hist1)):
+		for j in range(len(hist1[i])):
+			dist += (float(hist1[i][j] - hist2[i][j]))**2
+
+	dist = math.sqrt(dist)
+
+	return dist
+
 
 if __name__ == "__main__":
 
 	NUM_BINS = 8
 
-	path = 'ImClass/'
+	# Lists to hold the histograms for the images and their respective class labels
+	train_img_histograms = []
+	train_img_labels = []
 
-	img = cv2.imread('ImClass/coast_test1.jpg')
+	# Get a list of all file paths to training images in ImClass directory
+	train_img_dir = glob.glob('ImClass/*train*.jpg')
 
-	hist = getHist(img, NUM_BINS)	
-	print(verifyHist(hist, img))
+	print("Getting training data histograms")
 
-	cv2.imshow("test", img)
-	cv2.waitKey(0)
+	# Loop through the training images in the directory
+	for filename in train_img_dir:
+		# Read in the image
+		img = cv2.imread(filename)
+
+		# Compute the histogram
+		hist = getHist(img, NUM_BINS)
+
+		# Append the histogram to the training img list
+		train_img_histograms.append(hist)
+
+		# Check which class the image is and append that class to labels list
+		if "coast" in filename:
+			train_img_labels.append("coast")
+			# print("coast")
+		elif "forest" in filename:
+			train_img_labels.append("forest")
+			# print("forest")
+		elif "insidecity" in filename:
+			train_img_labels.append("insidecity")
+			# print("insidecity")
+
+		# cv2.imshow(filename, img)
+		# cv2.waitKey(0)
+
+	# Get a list of all file paths to test images in ImClass directory
+	test_img_dir = glob.glob('ImClass/*test*.jpg')
+
+	# print(test_img_dir)
+	# print(len(test_img_dir))
+	# exit()
+
+	# Lists to hold the file names for the images and their respective class labels
+	test_img_names = []
+	test_img_labels = []
+
+	print("Classifying test images")
+
+	# Loop through the testing images in the directory
+	for filename in test_img_dir:
+		# Read in the image
+		img = cv2.imread(filename)
+
+		# Compute the histogram
+		hist = getHist(img, NUM_BINS)
+
+		# Figure out which histogram in the training list has the smallest distance to the current image
+		best_dist = 9999999999 # Very large best distance value
+		idx = 0
+		# print("Yeet: " + str(len(train_img_histograms)))
+		# exit()
+		for i in range(len(train_img_histograms)):
+			new_dist = getEuclidDist(hist, train_img_histograms[i])
+			# print(new_dist)
+
+			# If a new smallest distance has been found save the index of the image and update best_dist
+			if new_dist < best_dist:
+				best_dist = new_dist
+				idx = i
+		
+		test_img_names.append(filename)
+		test_img_labels.append(train_img_labels[idx])
+
+		print(str(filename[8:]) + " has been assigned to class " + str(train_img_labels[idx]))
+
+		cv2.imshow(filename[8:], img)
+		cv2.waitKey(0)
+
+
